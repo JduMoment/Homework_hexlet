90b03f362f692b727191cf8ae398ed16b2b38af1
Kuragin Ivan <curagin.vanya@yandex.ru>
2023-11-20 08:28:54 +0300

    finish 3 step

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,6 @@
-from package_name import capitalize
+from package_name.capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello'
 
 print('Все тесты пройдены!')
